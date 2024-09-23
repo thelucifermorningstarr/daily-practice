e195054477736c9f7a1fa093bf3c27354c62f1fb
thelucifermorningstarr <creaturehackop@gmail.com>
2024-09-23 09:12:49 +0000

    update

---
+++ leetcode_daily_problem/sep21.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<int> lexicalOrder(int n) {
+        vector<int> res(n);
+
+        int x=1;
+
+        for(int i=0; i<n; i++){
+            res[i]=x;
+            if(x*10>n){
+                if(x==n){
+                    x/=10;
+                }
+                x++;
+                while(x%10==0){
+                    x/=10;
+                }
+            }
+            else{
+                x*=10;
+            }
+        }
+    return res;
+    }
+};
